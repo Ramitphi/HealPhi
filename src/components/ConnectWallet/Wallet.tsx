cc7f7cdc25f5b988718abb3a8c8424994ac961ed
Ramitphi <18ucs144@lnmiit.ac.in>
2024-01-14 00:05:06 +0530

    first commit

---
+++ src/components/ConnectWallet/Wallet.tsx
@@ -0,0 +1,6 @@
+import { WagmiConfig, createConfig } from "wagmi";
+import {
+  ConnectKitProvider,
+  ConnectKitButton,
+  getDefaultConfig,
+} from "connectkit";
---
+++ src/components/common/Navbar.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+
+type NavbarProps = {};
+
+const Navbar: React.FC<NavbarProps> = () => {
+  return (
+    <header className="border-b border-custom-1 w-full absolute top-0 left-0 h-16 flex items-center px-10 z-10 text-slate-100 justify-between">
+      <p className="font-bold text-xl">Healphi</p>
+    </header>
+  );
+};
+export default Navbar;
